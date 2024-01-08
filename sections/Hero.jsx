89a88135c1694f030551b063e8346708e2c1f6eb
Khushi Agarwal <kagarwal1676@gmail.com>
2024-01-08 18:12:45 +0530

    planet images changed

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -16,15 +16,15 @@ const Hero = () => (
     >
       <div className="flex justify-center items-center flex-col relative z-10">
         <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-          Metaverse
+          The Incognito
         </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
         >
-          <h1 className={styles.heroHeading}>Ma</h1>
-          <div className={styles.heroDText} />
-          <h1 className={styles.heroHeading}>Ness</h1>
+          <h1 className={styles.heroHeading}>Realm</h1>
+          {/* <div className={styles.heroDText} />
+          <h1 className={styles.heroHeading}>Ness</h1> */}
         </motion.div>
       </div>
 
