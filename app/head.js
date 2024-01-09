40782e7af4d1c0c0807dc764e462f22705a15cf6
Viveknair6915 <viveknair6915@gmail.com>
2024-01-09 17:12:55 +0530

    minor fixes-1

--- app/head.js
+++ app/head.js
@@ -1,9 +1,11 @@
-const Head = () => (
-  <>
-    <title>HackStreet 2.0</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/hackstreetlogo.ico" />
-  </>
-);
+function Head() {
+  return (
+    <>
+      <title>HackStreet 2.0</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/hackstreetlogo.ico" />
+    </>
+  );
+}
 
 export default Head;
